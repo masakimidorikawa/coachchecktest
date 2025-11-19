695c3f9c62ff122dd63ef1b2c539bffa893c7508
midorikawa masaki <midokawa1119@gmail.com>
2025-11-19 23:43:23 +0900

    完成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts', [ContactController::class, 'store']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
